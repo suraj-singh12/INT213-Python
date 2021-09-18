022a1ff9db9f2c2a12641f2dc279f67254259cb6
suraj-singh12 <surajsingh5092@gmail.com>
2021-09-18 18:17:04 +0530

    unit2 finish, unit3 lists start & files reorganised on basis of units

--- substring.py
+++ substring.py
@@ -3,7 +3,7 @@ s = input("Enter a string: ")
 sub = input("Enter substring to be capitalized: ")
 
 if sub in s:
-    s = s.replace(sub,sub.upper())
+    s = s.replace(sub,sub.capitalize())
     print(s)
 else:
     print("Ops!", sub, "not found in", s)
